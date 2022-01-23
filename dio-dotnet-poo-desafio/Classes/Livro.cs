a3af1d6acccce2d278f8ceb1e843f16e14c153a6
Higor Franco dos Santos <98039053+HiguFS@users.noreply.github.com>
2022-01-23 19:47:07 -0300

    Add files via upload

---
+++ dio-dotnet-poo-desafio/Classes/Livro.cs
@@ -0,0 +1,60 @@
+using System;
+
+namespace DIO.Series
+{
+    public class Livro : EntidadeBase
+    
+    {
+        // Atributos
+		private Genero Genero { get; set; }
+		private string Titulo { get; set; }
+		private string Descricao { get; set; }
+		private int Edicao { get; set; }
+		private string Autor { get; set; }
+        private bool Excluido {get; set;}
+        
+
+        // Métodos
+		public Livro (int id, Genero genero, string titulo, string descricao, int edicao, string autor)
+		{
+			this.Id = id;
+			this.Genero = genero;
+			this.Titulo = titulo;
+			this.Descricao = descricao;
+			this.Edicao = edicao;
+			this.Autor = autor;
+            this.Excluido = false;
+            
+		}
+
+        public override string ToString()
+		{
+			// Environment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.environment.newline?view=netcore-3.1
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Edição: " + this.Edicao + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido  + Environment.NewLine;
+            retorno += "Autor: " + this.Autor;
+			return retorno;
+		}
+
+        public string retornaTitulo()
+		{
+			return this.Titulo;
+		}
+
+		public int retornaId()
+		{
+			return this.Id;
+		}
+        public bool retornaExcluido()
+		{
+			return this.Excluido;
+		}
+        public void Excluir() {
+            this.Excluido = true;
+        }
+    }
+}
---
+++ dio-dotnet-poo-desafio/Enum/Genero.cs
@@ -0,0 +1,20 @@
+namespace DIO.Series
+{
+	public enum Genero
+	{
+		Academicos = 1,
+		Acao = 2,
+		Autoajuda = 3,
+		Aventura = 4,
+		Biografia = 5,
+		Fantasia= 6,
+		Ficcao_Cientifica = 7,
+		Historico = 8,
+		HQs_e_Mangas = 9,
+		Humor = 10,
+		Misterio = 11,
+		Romance = 12,
+		Suspense = 13,
+		Terror = 14
+	}
+}
